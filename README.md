8e22196dc531398bc43429fc172be3196705d41b
QuynhXEM <quynh@xem.edu.vn>
2025-03-17 17:50:25 +0700

    source

---
+++ README.md
@@ -0,0 +1,14 @@
+# Wrapped Viplus Coin (WVPC)
+
+![Word Of Life Coin](https://wolbank.org/default/asset/img/logo.png)
+
+
+- Name: Word Of Life 
+- Ticker: WOL
+- Contract: 0xcF91421f88FbfF0B34466c1baE2BD4bc4CF032DA
+- Contract Source Code Verified: https://bscscan.com/token/0xcF91421f88FbfF0B34466c1baE2BD4bc4CF032DA#code
+- Max Total Supply: 200,000,000 WOL
+- Decimals: 18
+
+
+For more information, please visit: Complete Soon !
---
+++ src/Token.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Token {
+    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) public allowance;
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 200000000 * 10**uint256(decimals);
+    string public name = "Word Of Life";
+    string public symbol = "WOL";
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+
+    function balanceOf(address owner) public view returns (uint256) {
+        return balances[owner];
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(balanceOf(msg.sender) >= value, "balance too low");
+        balances[msg.sender] -= value;
+        balances[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(balanceOf(from) >= value, "balance too low");
+        require(allowance[from][msg.sender] >= value, "allowance too low");
+        balances[from] -= value;
+        balances[to] += value;
+        emit Transfer(from, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) public returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+}
